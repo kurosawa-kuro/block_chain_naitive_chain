c7234e7775e563a7ea317a10c41e70feab10f424
Toshifumi Kurosawa <kuromailserver@gmail.com>
2018-07-04 03:08:12 +0900

    ブロックチェーン P2Pサーバーを構築して試す方法

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ npm i crypto-js --save
 npm i jest --save-dev
 npm i express --save-dev
 npm i body-parser --save-dev
+npm i ws --save-dev
 ```
 
 ## 動作テスト コマンド
@@ -16,8 +17,17 @@ npm i body-parser --save-dev
 npm run dev-test
 ```
 
-### jestを使ったテストコード コマンド 
+## jestを使ったテストコード コマンド 
 
 ```
 npm run test
+```
+
+## サーバー起動 コマンド 
+
+```
+npm run dev
+
+HTTP_PORT=3002 P2P_PORT=5002 PEERS=ws://localhost:5001 npm run dev
+HTTP_PORT=3003 P2P_PORT=5003 PEERS=ws://localhost:5001,ws://localhost:5002 npm run dev
 ```
--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const BlockChain = require('../blockchain');
+const blockChain = require('../blockchain');
+const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
@@ -8,7 +9,8 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const bc = new BlockChain();
+const bc = new blockChain();
+const p2pServer = new P2pServer(bc);
 
 app.get('/blocks',(req, res) => {
     res.json(bc.chain);
@@ -17,8 +19,9 @@ app.get('/blocks',(req, res) => {
 app.post('/mine',(req, res) => {
     const block = bc.addBlock(req.body.data);
     console.log(`ブロックが追加されました。${block.toString()}` );
-    
     res.redirect('/blocks');
 })
 
 app.listen(HTTP_PORT, () => console.log(`Listen ${HTTP_PORT}`));
+
+p2pServer.listen();
