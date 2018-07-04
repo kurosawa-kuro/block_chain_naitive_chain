88656ec2105ce2d423c826008b5c1599dcd0fdb7
Toshifumi Kurosawa <kuromailserver@gmail.com>
2018-07-04 19:02:34 +0900

    Proof of work ブロック採掘でブロックデータの正当性を保証する

--- blockchain/block.js
+++ blockchain/block.js
@@ -2,11 +2,12 @@ const SHA256 = require('crypto-js/sha256');
 const { DIFFICULTY } = require('../config');
 
 class Block{
-    constructor(timestamp, lastHash, hash, data){
+    constructor(timestamp, lastHash, hash, data, nonce){
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data;
+        this.nonce = nonce;
     }
 
     toString(){
@@ -14,29 +15,36 @@ class Block{
             Timestamp = ${this.timestamp};
             LastHash = ${this.lastHash.substring(0,10)};
             hash = ${this.hash.substring(0,10)};
+            nonce = ${this.nonce};
             data = ${this.data};
         `
     }
 
     static genesis(){
-        return new this("timestamp","-----","x1x2x3x4",[]);
+        return new this("timestamp","-----","x1x2x3x4",[],0);
     }
 
     static mineBlock(lastBlock, data){
-        const timestamp = Date.now();
         const lastHash = lastBlock.hash;
-        const hash = Block.hash(timestamp, lastHash, data);
+        let timestamp,hash;
+        let nonce = 0;
 
-        return new Block(timestamp, lastHash, hash, data);
+        do {
+            nonce++;
+            timestamp = Date.now();
+            hash = Block.hash(timestamp, lastHash, data, nonce);
+        } while (hash.substring(0,DIFFICULTY) !== '0'.repeat(DIFFICULTY));
+
+        return new Block(timestamp, lastHash, hash, data, nonce);
     }
 
-    static hash(timestamp, lastHash, data){
-        return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    static hash(timestamp, lastHash, data, nonce){
+        return SHA256(`${timestamp}${lastHash}${data}${nonce}`).toString();
     }
 
     static blockHash(block){
-        const { timestamp, lastHash, data } = block;
-        return Block.hash(timestamp, lastHash, data);
+        const { timestamp, lastHash, data, nonce } = block;
+        return Block.hash(timestamp, lastHash, data, nonce);
     }
 }
 
--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const { DIFFICULTY } = require('../config');
 
 describe('Block',() =>{
 
@@ -16,5 +17,10 @@ describe('Block',() =>{
 
     it('hash test', () =>{
         expect(block.lastHash).toEqual(lastBlock.hash);
+    });
+
+    it('指定難易度のハッシュ値生成テスト', () =>{
+        expect(block.hash.substring(0,DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
+        console.log(block.toString());
     });
 })
